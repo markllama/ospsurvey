cbd33c77645a93515a7888873bbc5ce65fbdfa91
Mark Lamourine <mark.lamourine@rackspace.com>
2019-11-29 13:30:03 -0500

    use auth for v2 and v3

--- python/bin/keystone_auth_sample.py
+++ python/bin/keystone_auth_sample.py
@@ -5,21 +5,35 @@ from __future__ import print_function
 import os
 
 import keystoneauth1.session
-import keystoneauth1.identity.v3
+import keystoneauth1.identity
 
-# Only v3 credentials
-ks_credentials = {
-    "auth_url": os.getenv('OS_AUTH_URL'),
-    "username": os.getenv('OS_USERNAME'),
-    "password": os.getenv('OS_PASSWORD'),
-    "user_domain_name": os.getenv('OS_USER_DOMAIN_NAME'),
-    "project_name": os.getenv('OS_PROJECT_NAME'),
-    "project_domain_name": os.getenv('OS_PROJECT_DOMAIN_NAME')
-}
+import keystoneclient.client
 
-if __name__ == "__main__":
-    
-    ks_auth = keystoneauth1.identity.v3.Password(**ks_credentials)
+auth_env_keys = [
+    'OS_AUTH_URL', 'OS_USERNAME', 'OS_PASSWORD',
+    'OS_TENANT_NAME', 'OS_TENANT_ID',
+    'OS_PROJECT_NAME', 'OS_PROJECT_ID',
+    'OS_USER_DOMAIN_NAME', 'OS_USER_DOMAIN_ID',
+    'OS_PROJECT_DOMAIN_NAME', 'OS_PROJECT_DOMAIN_ID'
+]
+
+def get_os_creds():
+    return { k.lower()[3:]:os.environ[k] for k in os.environ.keys() if k in auth_env_keys }
+
+def os_session(os_creds):
+    ks_auth = keystoneauth1.identity.Password(**os_creds)
     ks_session = keystoneauth1.session.Session(auth=ks_auth)
 
-    # now pass the session to an Openstack API client like ironicclient.client()
+    return ks_session
+
+if __name__ == "__main__":
+
+  ks_creds = get_os_creds()
+  ks_session = os_session(ks_creds)
+
+  ks_auth_version = int(os.getenv('OS_IDENTITY_API_VERSION')) if 'OS_IDENTITY_API_VERSION' in os.environ.keys() else 2
+
+  ks = keystoneclient.client.Client(ks_auth_version, session=ks_session, auth_url=ks_creds['auth_url'])
+
+  users = ks.users.list()
+  print(users)
