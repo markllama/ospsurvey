2fdf0d1b719b4a36cf5ff2143324c1a40c72e5bb
Mark Lamourine <mark.lamourine@rackspace.com>
2019-12-04 09:25:33 -0500

    starting to create a module and unittests

--- python/src/version.py
+++ python/src/version.py
@@ -18,6 +18,7 @@ import re
 import subprocess
 
 
+# In OSP 13+ you can read this file to get OSP version and upstream release
 default_version_file = "/etc/rhosp-release"
 
 # heat_template_version
@@ -26,8 +27,12 @@ default_version_file = "/etc/rhosp-release"
 #        : queens     - OSP13
 
 # repos numbers
-# yum info installed python-tripleoclient | grep 'From repo' | cut -d: -f
+# yum info -C installed python-tripleoclient | grep 'From repo' | cut -d: -f
 
+#
+# The OSP packages are installed from specific repositories.  You can query a
+# package and then lookup the repo name in this table.
+#
 yum_repos = {
     "osp9-rhel-x86_64-server-7-ost-9-director": "9",
     "rhel-7-server-openstack-10-rpms": "10",
@@ -40,7 +45,7 @@ default_osp_package = "python_tripleoclient"
 
 def version(version_file=default_version_file):
     """
-    TBD
+    Query and report the OSP version installed.
     """
 
     if os.path.exists(version_file):
@@ -51,7 +56,9 @@ def version(version_file=default_version_file):
 
 def version_from_file(version_file):
     """
-    TBD
+    From OSP 13 on the version release and build numbers are reported in a
+    file: /etc/rhosp-release.
+    It reports the OSP version and the upstream release name
     """
 
     version_pattern = "Red Hat OpenStack Platform release ([\d.]+) \((.*)\)"
@@ -67,21 +74,35 @@ def version_from_file(version_file):
     return release_match.groups()
 
 def version_from_repo_name(package_name=default_osp_package):
-
-    info_command_template = "yum info installed {}"
+    """
+    The source repo name is stored with the package.
+    Query the package information only for the installed package and
+    work from cached data to avoid side effects to the host.
+    The "From repo" line indicates where the package came from.
+    That name should indicate the major version of OSP from a table.
+    """
+    
+    info_command_template = "yum info installed --cacheonly {}"
     info_command = info_command_template.format(package_name)
 
     # subprocess wants a list of arguments, not a single string
     package_info = subprocess.check_output(info_command.split())
 
     # check_output returns the stdout as a single string
+    # Convert to an array of lines
     package_lines = package_info.split("\n")
 
     # should check that there is EXACTLY one line
-    repo_lines = [line for line in package_lines if line.startswith("From repo ")]
-    repo_name = repo_lines[0].split(':')[1].strip()
+    repo_lines = \
+        [line for line in package_lines if line.startswith("From repo ")]
 
-    version_string = yum_repos[repo_name] if repo_name in yum_repos.keys() else None
+    # "From repo    : <repo name>"
+    # Get the value and remove white space.
+    repo_name = repo_lines[0].split(':')[1].strip()
+    
+    version_string = yum_repos[repo_name] \
+        if repo_name in yum_repos.keys() \
+           else None
 
     return version_string
 
---
+++ python/unittest/test_version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import unittest
+
+import version
+
+class TestOspVersion(unittest.TestCase):
+
+    def test_version_from_file(self):
+        pass
+
+    def test_version_from_rpm_repo(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
