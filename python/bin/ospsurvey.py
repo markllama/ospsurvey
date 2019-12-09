92e40a9e932ed737d67ee4187b8dd40d0b6abab0
Mark Lamourine <mark.lamourine@rackspace.com>
2019-12-09 15:39:44 -0500

    skeleton survey

---
+++ python/bin/ospsurvey.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+from __future__ import print_function
+"""
+Survey an OSP system.
+Determine the installed version and query a number of stats and configurations to report
+the current status of the cluster.
+
+Output data either as a pretty table or as JSON structured data for consumption by a database
+or analysis tool.
+"""
+
+osp_status = {}
+
+if __name__ == "__main__":
+
+
+  # create session
+  
+  # create functions with session
+
+  # call functions to fill structure
+
+  # detect version
+
+  # query overstack name/install/update time
+
+  # query list of overstack nodes
+  
+  # query overstack node types
+
+  # query hardware make/model
+  
+  # query compute node local disk
+
+  # report status
