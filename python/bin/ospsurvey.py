665255acac4dfff03617be76ea219e05dcb89bc8
Mark Lamourine <mark.lamourine@rackspace.com>
2019-12-10 11:57:38 -0500

    add env checking

--- python/bin/ospsurvey.py
+++ python/bin/ospsurvey.py
@@ -17,8 +17,9 @@ import novaclient
 
 osp_status = {}
 
-def get_osp_api_versions():
-
+def get_osp_module_versions():
+  """
+  """
   versions = {
     'keystoneauth1': keystoneauth1.__version__,
     'keystoneclient': keystoneclient.__version__,
@@ -27,11 +28,43 @@ def get_osp_api_versions():
 
   return versions
 
+osp_varmap = {
+  'OS_IDENTITY_API_VERSION': 'identity_api_version',
+
+  'OS_CLOUDNAME': 'cloudname',
+  
+  'OS_AUTH_URL': 'auth_url',
+  'OS_PASSWORD': 'password',
+
+  'OS_USERNAME': 'username',
+  'OS_USER_DOMAIN_NAME': 'user_domain_name',
+
+  'OS_TENANT_NAME': 'tenant_name',
+
+  'OS_PROJECT_NAME': 'project_name',
+  'OS_PROJECT_DOMAIN_NAME': 'project_domain_name',
+
+  'COMPUTE_API_VERSION': 'compute_api_version',
+  'NOVA_VERSION': 'nova_version',
+
+  'OS_BAREMETAL_API_VERSION': 'baremetal_api_version',
+  'IRONIC_API_VERSION': 'ironic_api_version',
+
+  'OS_IMAGE_API_VERSION': 'image_api_version'
+}
+
+def get_osp_envvars():
+  envvars = {}
+  for k,v in osp_varname_map.items():
+    envvars[v] = os.environ[k] if k in os.environ else None
+
+  return envvars
+  
 if __name__ == "__main__":
 
   
   # report OSP Library Versions
-  osp_status['python_versions'] = get_osp_api_versions()
+  osp_status['python_versions'] = get_osp_module_versions()
 
   # create session
   
