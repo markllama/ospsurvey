64645d1bd446c65714656149340ba75ed98c00af
Mark Lamourine <markllama@gmail.com>
2020-01-02 17:51:51 +0000

    start checking return results from probes

--- tests/test_probes.py
+++ tests/test_probes.py
@@ -22,11 +22,12 @@ class TestProbes(unittest.TestCase):
   def test_services_get(self):
     s = ospsurvey.probes.services.get_service('nova')
 
-
   def test_endpoints_list(self):
 
     s = ospsurvey.probes.endpoints.list_endpoints()
 
+    self.assertGreater(len(s), 0)
+    
   def test_endpoints_get(self):
     s = ospsurvey.probes.endpoints.get_endpoint('f776829dcb7e4f25a2aadd588246f9c9')
 
