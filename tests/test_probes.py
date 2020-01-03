eb4145b3872b16ea94fa1c5f9bdb9ca09a06aab1
Mark Lamourine <markllama@gmail.com>
2020-01-03 13:35:57 +0000

    add comment lines to probe tests

--- tests/test_probes.py
+++ tests/test_probes.py
@@ -15,26 +15,41 @@ class TestProbes(unittest.TestCase):
     pass
   
   def test_services_list(self):
-
+    """
+    Check that the list_services function returns a valid list
+    of ServiceClass objects
+    """
+    
     s = ospsurvey.probes.services.list_services()
     self.assertGreater(len(s), 0, "No services present or reported")
 
 
   def test_services_get(self):
+    """
+    Check that the get_service function returns an object and has valid
+    structure
+    """
     services = ospsurvey.probes.services.list_services()
     s = ospsurvey.probes.services.get_service(services[0].Name)
 
   def test_endpoints_list(self):
-
+    """
+    Check that the list_endpoints function returns more than one element
+    and those are valid objects
+    """
     e = ospsurvey.probes.endpoints.list_endpoints()
 
     self.assertGreater(len(e), 0, "No endpoints present or reported")
 
   def test_endpoints_get(self):
+    """
+    Check that the get_endpoints returns a single valid EndpointClass object
+    """
     endpoints = ospsurvey.probes.endpoints.list_endpoints(interface="public")
     e = ospsurvey.probes.endpoints.get_endpoint(endpoints[0].ID)
 
   def test_nodes_list(self):
+    
     n = ospsurvey.probes.nodes.list_nodes()
     self.assertGreater(len(n), 0, "No nodes present or reported")
 
