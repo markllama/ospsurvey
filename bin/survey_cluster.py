51d1558920112e9a547e93640104d9d3e3a73c70
Mark Lamourine <markllama@gmail.com>
2020-01-07 14:36:51 +0000

    find yaml template files

--- bin/survey_cluster.py
+++ bin/survey_cluster.py
@@ -114,6 +114,9 @@ def read_profile_hints(template_dir=os.path.join(os.environ['HOME'], "templates"
   # find any hints
   for filename in template_files:
     template_data = yaml.load(open(os.path.join(template_dir, filename)))
+
+    # check if it has hints.
+    hints = {k,v for k,v in template_data['parameter_defaults'] if k.endswith("Hints")}
     
   
 
