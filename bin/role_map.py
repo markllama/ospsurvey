bf76d0a9536afda0944d20111b6412d40c91a982
Mark Lamourine <markllama@gmail.com>
2020-01-08 16:01:30 +0000

    create role assignment script for dev/test

--- bin/role_map.py
+++ bin/role_map.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
   # list the stacks and get the environment
   stacks = ospsurvey.probes.stack.list_stacks()
-  stack_env = ospsurvey.probes.stack.get_environment(stacks[0].Stack_Name)
+  stack_name = stacks[0].Stack_Name
+  stack_env = ospsurvey.probes.stack.get_environment(stack_name)
 
   # find all of the hints
 
@@ -18,7 +19,8 @@ if __name__ == "__main__":
 
   print(hints)
 
-  node_patterns = {v['capabilities:node']:re.sub('\SchedulerHints$','',k) for (k,v) in hints.items()}
+  
+  node_patterns = {stack_name + '-' + v['capabilities:node']:re.sub('\SchedulerHints$','',k) for (k,v) in hints.items()}
 
   print(node_patterns)
   
