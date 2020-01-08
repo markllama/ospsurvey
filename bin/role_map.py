e3c01a7656262c15bc43868584f2060c984919b0
Mark Lamourine <markllama@gmail.com>
2020-01-08 18:23:06 +0000

    create role assignment script for dev/test

--- bin/role_map.py
+++ bin/role_map.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
   nodes = ospsurvey.probes.nodes.list_nodes()
 
-  node_tags = [{"Name":n.Name, "Properties":n.Properties} for n in nodes]
+  node_tags = [{"Name":n.Name, "Capabilities":n.Properties['capabilities']} for n in nodes]
 
   print(node_tags)
   
