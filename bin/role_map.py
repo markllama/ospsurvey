773def4c37b8d5df74aec5dde4d3818c6e47c666
Mark Lamourine <markllama@gmail.com>
2020-01-08 21:44:24 +0000

    create role assignment script for dev/test

--- bin/role_map.py
+++ bin/role_map.py
@@ -30,11 +30,11 @@ if __name__ == "__main__":
 
   # find all of the hints
 
-  hints = {k:v['capabilities:node'] for (k,v) in stack_env.parameter_defaults.items() if k.endswith("Hints")}
+  hints = {re.sub('SchedulerHints$', k):v['capabilities:node'] for (k,v) in stack_env.parameter_defaults.items() if k.endswith("Hints")}
 
   print(hints)
   
-  node_patterns = {re.sub('%index%', '\d+$', v):re.sub('\SchedulerHints$','',k) for (k,v) in hints.items()}
+  node_patterns = {re.sub('%index%', '\d+$', v):k for (k,v) in hints.items()}
 
   print(node_patterns)
 
