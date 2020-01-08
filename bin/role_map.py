c897ce5e7ea8a6be8914067c9eb40735a4e4cebe
Mark Lamourine <markllama@gmail.com>
2020-01-08 15:23:47 +0000

    create role assignment script for dev/test

--- bin/role_map.py
+++ bin/role_map.py
@@ -6,5 +6,5 @@ import ospsurvey.probes.stack
 
 if __name__ == "__main__":
 
-  stacks = ospsurvey.probes.stack.list_stacks
-  stack_env = ospsurvey.probes.stack.get_environment(stack[0].Stack_Name)
+  stacks = ospsurvey.probes.stack.list_stacks()
+  stack_env = ospsurvey.probes.stack.get_environment(stacks[0].Stack_Name)
