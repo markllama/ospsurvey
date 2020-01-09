d9d4c65ee88aedf1e67166b8ef8a8e6b183a4574
Mark Lamourine <markllama@gmail.com>
2020-01-09 22:31:04 +0000

    start creating a single command to report server role relationships

--- bin/server-role.py
+++ bin/server-role.py
@@ -106,6 +106,7 @@ if __name__ == "__main__":
 
     # find all of the hints
     hints = {re.sub('SchedulerHints$', '', k):v['capabilities:node'] for (k,v) in stack_env.parameter_defaults.items() if k.endswith("Hints")}
+    node_patterns = {re.sub('%index%', '\d+$', v):k for (k,v) in hints.items()}
 
 
 
