f7922b76e84ee0bc8caea7c25cdfb37e2e6e84b1
Mark Lamourine <markllama@gmail.com>
2020-01-09 17:32:24 +0000

    create role assignment script for dev/test

--- bin/role_map.py
+++ bin/role_map.py
@@ -65,6 +65,6 @@ if __name__ == "__main__":
   # Now assocate nodes to servers and we'll have a role map for servers
   # For each node, look up the instance UID, then find that instance and
   # get the display name.  Associate the display name with the role
-  server_roles = {node_role(n,servers):node_roles[n.Name] for n in nodes}
+  server_roles = {get_server_from_node(n,servers):node_roles[n.Name] for n in nodes}
 
   print(server_roles)
