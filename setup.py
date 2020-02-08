77823498669029f352cc819ee36fe9b7d36a9e09
Mark Lamourine <markllama@gmail.com>
2020-02-08 00:05:51 +0000

    updated to 0.2.5, added bin/ospsurvey

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, 'README'), encoding='utf-8') as f:
   
 setup(
   name='ospsurvey',
-  version='0.2.4',
+  version='0.2.5',
   description='Survey an OpenStack Cluster',
   long_description=long_description,
   #long_description_content_type='text/markdown',
@@ -49,10 +49,10 @@ setup(
   },
 
   scripts=[
+    'bin/ospsurvey',
     'bin/server-role',
     'bin/service-checks',
-    'bin/check-updates',
-    'bin/rh-subscription'
+    'bin/check-updates'
   ],
   python_requires='>=2.7',
 
